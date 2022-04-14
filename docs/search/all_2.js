3dbb35e345b0262311c3424cd0933083d548df30
ZilongWang-UNO <jw565404751@gmail.com>
2022-04-14 14:08:07 -0500

    docs added

---
+++ docs/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['databaseaccess_0',['DatabaseAccess',['../interfacecom_1_1wecareinsurance_1_1dal_1_1_database_access.html',1,'com::wecareinsurance::dal']]],
+  ['deletecustomer_1',['deleteCustomer',['../classcom_1_1wecareinsurance_1_1controllers_1_1_customer_controller.html#a84863fed66593b31d7f2c4c0b3a8aaad',1,'com::wecareinsurance::controllers::CustomerController']]]
+];
---
+++ docs/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deletecustomer_0',['deleteCustomer',['../classcom_1_1wecareinsurance_1_1controllers_1_1_customer_controller.html#a84863fed66593b31d7f2c4c0b3a8aaad',1,'com::wecareinsurance::controllers::CustomerController']]]
+];
